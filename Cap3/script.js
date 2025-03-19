0cd6186c82d81fce08e0be19d6adc04a431a1df2
vicenteprogammer <vicentejunior4522@gmail.com>
2025-03-19 09:58:53 -0300

    Capitulo 3

---
+++ Cap3/script.js
@@ -0,0 +1,27 @@
+function mostrar(){
+    var inSexoM = document.getElementById("masculino");
+    var inSexoF = document.getElementById("feminino");
+    var inName = document.getElementById("nome");
+    var outNome = document.getElementById("outnome");
+
+    var inSexom = inSexoM.checked;
+    var inSexof = inSexoF.checked;
+    var nome = inName.value;
+
+    if(nomeome = "" || (inSexom == false && inSexof == false)){
+        alert("Digite o seu nome e preenchao sexo");
+        inName.focus()
+        return;
+    } else{
+        outNome.textContent = nome;
+    }
+}
+
+function limpar(){
+    location.reload()
+    document.getElementById("nome").focus()
+}
+var btMostrar = document.getElementById("mostrar");
+btMostrar.addEventListener("click", mostrar)
+var btLimpar = document.getElementById("limpar")
+btLimpar.addEventListener("click", limpar)
